b1061b94854993e7a726bfb146685043dec27b1d
cuongg-github <qc01696@gmail.com>
2024-07-03 21:16:54 +0700

    Add files via upload

---
+++ src/java/entity/OrderProduct.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class OrderProduct {
+    private Orders orderid;
+    private Product productid;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(Orders orderid, Product productid) {
+        this.orderid = orderid;
+        this.productid = productid;
+    }
+
+    public Orders getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Orders orderid) {
+        this.orderid = orderid;
+    }
+
+    public Product getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Product productid) {
+        this.productid = productid;
+    }
+    
+}
